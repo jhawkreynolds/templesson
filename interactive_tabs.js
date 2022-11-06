fbea21d88a20ea195f390f6514ec03b9201423a5
jhawkreynolds <112728229+jhawkreynolds@users.noreply.github.com>
2022-11-06 16:45:00 -0600

    Moved tab JS code to a script file

---
+++ interactive_tabs.js
@@ -0,0 +1,48 @@
+class Tabs {
+    constructor(source) {
+      this.visibleSection = 0;
+      this.path = document.getElementById(source);
+      this.size = document.getElementById(source).children.length;
+    }
+  }
+  
+  function hide(tab)
+  {
+      for(let i = 0; i < tab.size; i++)
+      {
+          if(i == tab.visibleSection)
+          {
+              tab.path.children[i].style.display = "";        // make not hidden
+          }
+          else    // if section should not be visible
+          {
+              tab.path.children[i].style.display = "none";    // make hidden, don't make space for this
+          }
+      }
+  }
+  
+  function previous(tab)
+  {
+      if(tab.visibleSection == 0)
+      {
+          tab.visibleSection = tab.size - 1;
+      }
+      else
+      {
+          tab.visibleSection -= 1;
+      }
+      hide(tab);
+  }
+  
+  function next(tab)
+  {
+      if(tab.visibleSection == tab.size - 1)
+      {
+          tab.visibleSection = 0;
+      }
+      else
+      {
+          tab.visibleSection += 1;
+      }
+      hide(tab);
+  }
